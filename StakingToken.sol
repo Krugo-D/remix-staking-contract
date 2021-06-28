e1c4a1d1f86f40345a07e8dc43c97e400194387f
fondofdogs <fondofdogs@protonmail.com>
2021-06-28 14:28:36 +0700

    commit

--- StakingToken.sol
+++ StakingToken.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract StakingToken is ERC20, ERC20Detailed {
+    
     constructor() ERC20Detailed("StakingToken", "ST", 18) public {
         _mint(msg.sender, 1000000000);
     }
